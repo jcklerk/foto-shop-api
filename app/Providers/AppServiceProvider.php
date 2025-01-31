3c3429a02028ca869bc661c888a07950975a553a
jcklerk <justin@klerk.nl>
2025-01-31 19:12:59 +0100

    fix ?

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (config('app.env') != 'local') {
+            resolve(\Illuminate\Routing\UrlGenerator::class)->forceScheme('https');
+        }
+        
     }
 }
